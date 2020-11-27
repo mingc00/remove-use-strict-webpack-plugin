5595843415cfd7416bd0f8103853e50593c135fe
Mingc <ciffel@gmail.com>
2020-11-27 19:21:29 +0800

    support webpack v5

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 const { ModuleFilenameHelpers } = require('webpack');
 
-class RemoveStrictTemplatePlugin {
-  constructor(options) {
-    this.options = options || {};
-  }
-
-  apply(moduleTemplate) {
-    const USE_STRICT_STMT = '"use strict";\n';
-    const { sourceFilter = () => true } = this.options;
-    moduleTemplate.hooks.render.tap('RemoveStrictTemplatePlugin', (source, module) => {
-      if (ModuleFilenameHelpers.matchObject(this.options, module.resource)) {
-        const index = source.children.indexOf(USE_STRICT_STMT);
-        if (index > -1 && sourceFilter(source) === true) {
-          source.children.splice(index, 1, ';\n');
-        }
-      }
-      return source;
-    });
-  }
-}
-
 class RemoveUseStrictPlugin {
   constructor(options) {
     this.options = options;
@@ -27,8 +7,18 @@ class RemoveUseStrictPlugin {
 
   apply(compiler) {
     const pluginName = 'RemoveUseStrictPlugin';
-    compiler.hooks.make.tap(pluginName, compilation => {
-      new RemoveStrictTemplatePlugin(this.options).apply(compilation.moduleTemplates.javascript);
+    compiler.hooks.compilation.tap(pluginName, (compilation) => {
+      const hooks = compiler.webpack.javascript.JavascriptModulesPlugin.getCompilationHooks(
+        compilation
+      );
+      const { sourceFilter = () => true } = this.options;
+      hooks.renderModuleContent.tap(pluginName, (moduleSource, module) => {
+        if (module.buildInfo.strict &&
+            ModuleFilenameHelpers.matchObject(this.options, module.resource) &&
+            sourceFilter(moduleSource) === true) {
+          module.buildInfo.strict = false;
+        }
+      });
     });
   }
 }
